470c3be095b20c22abc0c2cbe59504a1f358fb58
davidliberal0 <davidliberal0@gmail.com>
2022-11-16 17:41:48 -0500

    setup express boilerplate environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+// import express
+const express = require("express");
+// import dotenv for environment variable configuration
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+// initialize express
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
