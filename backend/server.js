b8b104af7d4e2eed1c60205b81cb6814453e1f02
davidliberal0 <davidliberal0@gmail.com>
2022-11-16 18:43:18 -0500

    add js file for handling requests and routes

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 5000;
 // initialize express
 const app = express();
 
+app.use("/api/goals", require("./routes/goalRoutes"));
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
