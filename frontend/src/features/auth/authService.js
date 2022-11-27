1d1ba8e916b07d82e3cddd681695ca0e149bdf9e
davidliberal0 <davidliberal0@gmail.com>
2022-11-26 21:37:53 -0500

    start redux setup

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const API_URL = "/api/users/";
+
+// Register User
+const register = async () => {
+  const response = await axios.post(API_URL, userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+const authService = {
+  register,
+};
+
+export default authService;
